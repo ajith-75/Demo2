c240eb9cbf51439fb9636ff1f8ad38a16e877e24
Prasanna Baskaran <167430207+Prasannab-ts1014@users.noreply.github.com>
2025-05-15 14:27:30 +0530

    Release update

--- obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7219cdaa321fb7ce976f6b81fe46e23a117c291")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad066a84e585ca383c581146e637a35566fbb14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
